f2f62ec93e9c36687c35be32e3152544a6ec2094
Shubham Chaudhary <shubhamyc7@gmail.com>
2021-10-22 15:56:14 +0530

    Git1 Updated

--- Git1.py
+++ Git1.py
@@ -88,3 +88,12 @@
   git push - u origin master
 
   ----------------------------------------------------------
+
+  touch README.md
+  git init
+  git add README.md
+  git commit -m "first commit"
+  git remote add origin git@github.com:alexpchin/<reponame>.git
+  git push -u origin master
+
+  ----------------------------------------------------------
